f433bedf7d98d3964ccbc5facc21f92ef3c9815e
farah <flabdi@student.unimelb.edu.au>
2025-10-07 13:59:23 +1100

    feat(SPRNT2-56): add nav-bar component to dashboard page

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,3 +1,24 @@
+'use client'
+
+import Navbar from '@/components/Navbar'; 
+import Link from "next/link";
+import Button from "@/components/Button";
+
 export default function AdminPage() {
-  return <h1 className="text-2xl font-bold">Admin page</h1>
+  return (
+    <div className="min-h-screen">
+      <Navbar 
+        actions={
+          <div className="flex items-center gap-3">
+            <Link href="/login">
+              <Button variant="outline">Log Out</Button>
+            </Link>
+          </div>
+        }
+      />
+      <main className="container mx-auto px-4 py-8">
+
+      </main>
+    </div>
+  )
 }
